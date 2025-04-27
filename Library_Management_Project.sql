af6a2638aa537c87c9f0792f68af6abea3735091
Jyoti Bawa <bawajyoti88@gmail.com>
2025-04-27 13:10:21 +0530

    Update Library_Management_Project.sql

--- Library_Management_Project.sql
+++ Library_Management_Project.sql
@@ -50,12 +50,12 @@ CREATE TABLE issued_status (
     issued_date DATE,
     issued_book_isbn VARCHAR(50),
     issued_emp_id VARCHAR(10)
-   /* FOREIGN KEY (issued_member_id)
+     FOREIGN KEY (issued_member_id)
         REFERENCES members (member_id),
     FOREIGN KEY (issued_emp_id)
         REFERENCES employees (emp_id),
     FOREIGN KEY (issued_book_isbn)
-        REFERENCES books (isbn) */
+        REFERENCES books (isbn) 
 );
 
 -- CREATING TALE RETURN STATUS
